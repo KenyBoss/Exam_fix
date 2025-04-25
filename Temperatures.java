92dc12364e827002bf21be5ddbcfb26efc1b4fc3
Keny <kenyjohnshamba@gmail.com>
2025-04-25 09:25:55 -0400

    Initial sharing of project

---
+++ Temperatures.java
@@ -0,0 +1,45 @@
+
+/**
+ * Write a description of class Tempatures here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Temperatures
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Tempatures
+     */
+    public Tempatures()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int CountFever(ArrayList<double> temperatures)
+    {
+       for-each(temperatures> 37.5);
+       temparatures.Count();
+    }
+    
+    
+    public void Max(){
+        max temperature = ArrayList<double> temperatures.max()   ;
+        System.out.println(max temperature);
+        
+    }
+    public void Sort()
+    {
+        Sort(ArrayList<double> temperatures);
+    }
+    
+}
---
+++ Twostrings.java
@@ -0,0 +1,36 @@
+// Question 24
+/**
+ * Write a description of class Twostrings here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Twostrings
+{
+
+    private int x;
+
+    /**
+     * Constructor for objects of class Twostrings
+     */
+    public Twostrings()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void Method() 
+    {
+      a = new String;
+      b = new String;
+      hi = a;
+      hello = b;
+      a.equal(b)? a:b ;
+    
+    }
+}
